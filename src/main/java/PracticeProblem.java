ea1d147b7dc070b37521f9ed285dea435d448c88
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-18 20:07:36 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,34 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static String pluralize(String word){
+		if (word.endsWith("ife")){
+			String suffix= "ives";
+			return suffix;
+		}
+		else if(word.endsWith("ey")){
+			String suffix= "eys";
+			return suffix;
+		}
+		else if (word.endsWith("y")){
+			String suffix= "ies";
+			return suffix;
+		}
+		else{
+			String suffix= "s";
+			return suffix;
+		}
+		}
+	public static int min(int num1, int num2, int num3){
+		if (num1<=num2 && num1<=num3){
+			return num1;
+			}
+		else if(num2<=num3){
+			return num2;
+		}
+		else{
+			return num3;
+		}
+		}
 
-}
+	}
